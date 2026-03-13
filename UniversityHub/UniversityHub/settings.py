97e068ce80b9536cdee5433f6b26f1627bba8f25
karthik104-code <cskarthik25cs@cce.edu.in>
2026-03-13 10:01:42 +0530

    Add files via upload

--- UniversityHub/UniversityHub/settings.py
+++ UniversityHub/UniversityHub/settings.py
@@ -119,3 +119,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/Academic/students/'
